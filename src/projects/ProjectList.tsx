cdaa557ba7dea6e1deef99ab4f29ee3622b0291a
jessamyn mctwigan <jmctwigan@tmrs.com>
2024-03-14 18:46:21 -0500

    lab 7 - map over list data to display in UI cards instead of json list

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -7,8 +7,20 @@ interface ProjectListProps {
 const ProjectList = ({ projects }: ProjectListProps) => {
 
     return (
-        <pre>{JSON.stringify(projects, null, ' ')}</pre>
-
+        <div className='row'>
+            {projects.map((project) => (
+                <div key={project.id} className='card'>
+                    <img src={project.imageUrl} alt={project.name}></img>
+                    <section className="section dark">
+                        <h5 className="strong">
+                            <strong>{project.name}</strong>
+                        </h5>
+                        <p>{project.description}</p>
+                        <p>Budget : {project.budget.toLocaleString()}</p>
+                    </section>
+                </div>
+            ))}
+        </div>
     )
 }
 
