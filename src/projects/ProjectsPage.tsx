293312292c0e0d18f31d79646531dd346fb00169
jessamyn mctwigan <jmctwigan@tmrs.com>
2024-03-14 18:32:23 -0500

    lab 6 - passing mock data to component as a prop

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,11 @@
-import { MOCK_PROJECTS } from "./MockProjects";
+import { MOCK_PROJECTS } from './MockProjects';
+import ProjectList from "./ProjectList";
 
 const ProjectsPage = () => {
     return (
         <>
             <h1>Projects</h1>
-            <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+            <ProjectList projects={MOCK_PROJECTS} />
         </>
     )
 }
