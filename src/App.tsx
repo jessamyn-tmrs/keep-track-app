095b61430e1c2dfaf50634be46f58e9f9f1b50d9
jessamyn mctwigan <jmctwigan@tmrs.com>
2024-05-03 17:03:34 -0500

    lab 20 - add routes and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import React from "react";
 import "../node_modules/mini.css/dist/mini-dark.min.css";
 import "./App.css";
 import ProjectsPage from "./projects/ProjectsPage";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import Homepage from "./home/Homepage";
 
 function App() {
   return (
-    <div className="container">
-      <ProjectsPage />
-    </div>
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route
+            path="/"
+            element={<Homepage />} />
+          <Route
+            path="/projects"
+            element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/home/Homepage.tsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    )
+}
+
+export default Homepage
